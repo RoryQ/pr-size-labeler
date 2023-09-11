8a76dc94caca2822e637920cdfc3bd8cbafadd78
Rory Quinn <quinn.rory@gmail.com>
2023-09-11 19:31:51 +1000

    test read inputs

--- go.mod
+++ go.mod
@@ -1,8 +1,24 @@
 module github.com/friendsofgo/pr-size-labeler
 
-go 1.15
+go 1.21
 
 require (
-	github.com/kelseyhightower/envconfig v1.4.0 // indirect
-	github.com/posener/goaction v0.1.4
+	github.com/google/go-github/v31 v31.0.0
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/kr/pretty v0.2.0
+	github.com/posener/goaction v1.2.1
+	gopkg.in/yaml.v3 v3.0.0-20191120175047-4206685974f2
+)
+
+require (
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.0 // indirect
+	github.com/kr/text v0.1.0 // indirect
+	github.com/posener/script v1.1.5 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+	golang.org/x/net v0.0.0-20190603091049-60506f45cf65 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	google.golang.org/appengine v1.6.5 // indirect
 )
--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/google/go-github/v31/github"
 	"github.com/kelseyhightower/envconfig"
+	"github.com/kr/pretty"
 	"github.com/posener/goaction"
 	"github.com/posener/goaction/actionutil"
 	"github.com/posener/goaction/log"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
@@ -21,8 +23,7 @@ func main() {
 		return
 	}
 
-	var config Config
-	err := envconfig.Process("input", &config)
+	config, err := getConfig()
 	if err != nil {
 		log.Fatalf("Required configuration is missing: %s", err)
 	}
@@ -32,70 +33,158 @@ func main() {
 		log.Fatalf("Error happened while getting event info: %s", err)
 	}
 
-	prSize := GetPrSize(config, event.PullRequest)
+	label, isXL := GetPrLabel(config, event.PullRequest)
 
 	ctx := context.Background()
 	gh := actionutil.NewClientWithToken(ctx, config.GitHubToken)
 
-	_, _, err = gh.IssuesAddLabelsToIssue(ctx, goaction.PrNum(), []string{string(prSize)})
+	err = replaceLabels(ctx, gh, config, label)
 	if err != nil {
 		log.Fatalf("Error happened while adding label: %s", err)
 	}
 
-	if prSize != XL {
+	if !isXL {
 		log.Debugf("Pull request successfully labeled")
 		return
 	}
 
 	_, _, err = gh.PullRequestsCreateComment(ctx, goaction.PrNum(), &github.PullRequestComment{
-		Body: github.String(config.MessageIfXL),
+		Body: github.String(config.Thresholds.MessageIfXL),
 	})
 
 	if err != nil {
 		log.Fatalf("Error happened while adding comment: %s", err)
 	}
 
-	if config.FailIfXL {
+	if config.Thresholds.FailIfXL {
 		log.Fatalf("PR size is XL, make it shorter, please!")
 	}
 
 	log.Debugf("Pull request successfully labeled")
 }
 
-func GetPrSize(config Config, pr *github.PullRequest) PrSize {
-	totalModifications := pr.GetAdditions() + pr.GetDeletions()
+func replaceLabels(ctx context.Context, gh *actionutil.Client, config Config, label string) error {
+	ghLabels, _, err := gh.IssuesListLabelsByIssue(ctx, goaction.PrNum(), nil)
+	if err != nil {
+		return err
+	}
+
+	confLabels := config.Thresholds.GetLabels()
+
+	for _, ghLabel := range ghLabels {
+		if contains(confLabels, ghLabel.GetName()) {
+			_, err = gh.IssuesRemoveLabelForIssue(ctx, goaction.PrNum(), ghLabel.GetName())
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	_, _, err = gh.IssuesAddLabelsToIssue(ctx, goaction.PrNum(), []string{label})
+	return err
+}
+
+func getConfig() (Config, error) {
+	config := Config{
+		Thresholds: defaultThresholds(),
+	}
+	err := envconfig.Process("input", &config)
+	if err != nil {
+		return Config{}, err
+	}
+
+	if err := yaml.Unmarshal([]byte(config.ThresholdsYAML), &config.Thresholds); err != nil {
+		return Config{}, err
+	}
+
+	if err := yaml.Unmarshal([]byte(config.ExcludePathsYAML), &config.ExcludePaths); err != nil {
+		return Config{}, err
+	}
+
+	pretty.Print(config)
+
+	return config, err
+}
+
+func GetPrLabel(config Config, pr *github.PullRequest) (string, bool) {
+	totalModifications := max(pr.GetAdditions(), pr.GetDeletions())
 
 	switch {
-	case totalModifications < config.XsMaxSize:
-		return XS
-	case totalModifications < config.SMaxSize:
-		return S
-	case totalModifications < config.MMaxSize:
-		return M
-	case totalModifications < config.LMaxSize:
-		return L
+	case totalModifications < config.Thresholds.XS.LessThan:
+		return config.Thresholds.XS.Label, false
+	case totalModifications < config.Thresholds.S.LessThan:
+		return config.Thresholds.S.Label, false
+	case totalModifications < config.Thresholds.M.LessThan:
+		return config.Thresholds.M.Label, false
+	case totalModifications < config.Thresholds.L.LessThan:
+		return config.Thresholds.L.Label, false
 	default:
-		return XL
+		return "", true
 	}
 }
 
 type PrSize string
 
-const (
-	XS PrSize = "size/xs"
-	S  PrSize = "size/s"
-	M  PrSize = "size/m"
-	L  PrSize = "size/l"
-	XL PrSize = "size/xl"
-)
-
 // Config is the data structure used to define the action settings.
 type Config struct {
-	GitHubToken string `envconfig:"GITHUB_TOKEN" required:"true"`
-	XsMaxSize   int    `envconfig:"XS_MAX_SIZE" default:"10"`
-	SMaxSize    int    `envconfig:"S_MAX_SIZE" default:"100"`
-	MMaxSize    int    `envconfig:"M_MAX_SIZE" default:"500"`
-	LMaxSize    int    `envconfig:"L_MAX_SIZE" default:"1000"`
-	FailIfXL    bool   `envconfig:"FAIL_IF_XL" default:"false"`
-	MessageIfXL string `envconfig:"MESSAGE_IF_XL" default:""`
+	GitHubToken      string `envconfig:"GITHUB_TOKEN" required:"true"`
+	Thresholds       Thresholds
+	ThresholdsYAML   string `envconfig:"THRESHOLDS"`
+	ExcludePaths     []string
+	ExcludePathsYAML string `envconfig:"EXCLUDE_PATHS"`
+}
+
+func defaultThresholds() Thresholds {
+	return Thresholds{
+		XS: Size{
+			LessThan: 10,
+			Label:    "size/xs",
+		},
+		S: Size{
+			LessThan: 100,
+			Label:    "size/s",
+		},
+		M: Size{
+			LessThan: 500,
+			Label:    "size/m",
+		},
+		L: Size{
+			LessThan: 1000,
+			Label:    "size/l",
+		},
+		FailIfXL:    false,
+		MessageIfXL: "This PR is too big. Please, split it.",
+	}
+}
+
+type Thresholds struct {
+	XS          Size   `yaml:"xs"`
+	S           Size   `yaml:"s"`
+	M           Size   `yaml:"m"`
+	L           Size   `yaml:"l"`
+	FailIfXL    bool   `yaml:"fail_if_xl"`
+	MessageIfXL string `yaml:"message_if_xl"`
+}
+
+func (t Thresholds) GetLabels() []string {
+	return []string{
+		t.XS.Label,
+		t.S.Label,
+		t.M.Label,
+		t.L.Label,
+	}
+}
+
+func contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+type Size struct {
+	LessThan int    `yaml:"less_than"`
+	Label    string `yaml:"label"`
 }
